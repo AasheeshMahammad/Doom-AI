4d6b5b1c271088ccac9e8547d5eba171d775f680
Aasheesh Mahammad <54064611+AasheeshMahammad@users.noreply.github.com>
2022-12-02 11:29:00 +0530

    Modified files

--- tester.py
+++ tester.py
@@ -1,18 +1,33 @@
 from stable_baselines3.ppo import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
+import time
 
 
 class Tester():
-    def __init__(self,model_path,env,render=True, config='./ViZDoom/scenarios/deadly_corridor.cfg'):
+    def __init__(self,model_path,env,render=True, map_name="basic",config='./ViZDoom/scenarios/deadly_corridor.cfg'):
         self.model = PPO.load(model_path)
-        self.env = env(render,config=config)
+        self.env = env(render,map_name)
     
-    def test(self,num_episodes=10):
-        rewards,steps= evaluate_policy(self.model,self.env,n_eval_episodes=num_episodes,return_episode_rewards=True)
-        mean_reward = sum(rewards)/len(rewards)
+    def do(self, num_episodes):
+        for episode in range(num_episodes): 
+            obs = self.env.reset()
+            done = False
+            total_reward = 0
+            while not done: 
+                action, _ = self.model.predict(obs)
+                obs, reward, done, info = self.env.step(action)
+                #time.sleep(0.10)
+                total_reward += reward
+            print('Total Reward for episode {} is {}'.format(episode+1, total_reward))
+            time.sleep(1)
+
+    def test(self,num_episodes=5):
+        #rewards,steps= evaluate_policy(self.model,self.env,n_eval_episodes=num_episodes,return_episode_rewards=True)
+        self.do(num_episodes)
+        '''mean_reward = sum(rewards)/len(rewards)
         for epi in range(len(rewards)):
             print(f"Reward for episode {epi+1} is {rewards[epi]}")
-        print(f"Mean reward is {mean_reward}")
+        print(f"Mean reward is {mean_reward}")'''
         
 
     def close(self):
--- wrapper.py
+++ wrapper.py
@@ -5,34 +5,54 @@ from matplotlib import pyplot as plt
 import cv2
 import numpy as np
 from stable_baselines3.common import env_checker
+import cv2
 
 class DoomEnvironment(Env): 
     
-    def __init__(self, render=False, config='./ViZDoom/scenarios/basic.cfg'): 
+    def __init__(self, render=False, map_name="basic", config=None): 
         super().__init__()
         self.game = DoomGame()
+        self.map_name=map_name
+        if config == None:
+            config =f"./ViZDoom/scenarios/{map_name}.cfg"
         self.game.load_config(config)
         self.game.set_window_visible(render)
+        self.game.set_labels_buffer_enabled(True)
         self.game.init()
-        state = self.game.get_state().screen_buffer
+        state = self.game.get_state().labels_buffer
         original_shape = state.shape
         final_shape = self.reduce_size(state).shape
         self.observation_space = Box(low=0, high=255, shape=final_shape, dtype=np.uint8)
         self.buttons = self.game.get_available_buttons()
+        #print(len(self.buttons))
         self.action_space = Discrete(len(self.buttons))
+        self.reset_variables()
+        
+    def reset_variables(self):
         self.damage_taken = 0
         self.hitcount = 0
-        self.ammo = 52
+        self.damageCount = 0
+        self.ammo = None
+        self.itemCount = 0
+        self.armor = 0
+        self.health = 100
+        self.position = [None, None]
+        self.weapons = [None for _ in range(6)]
+        self.prevWeapon = None
     
-    def get_reward(self, reward):
+    def get_reward(self, reward,action):
         game_variables = self.game.get_state().game_variables
         #print(game_variables)
-        if len(game_variables) == 1:
+        size = len(game_variables)
+        #print(self.map_name)
+        if size == 1:
             self.ammo = game_variables[0]
             return reward
-        else:
+        elif self.map_name == "deadly_corridor":
             movement_reward = reward
             health, damage_taken, hitcount, ammo = game_variables
+            if self.ammo == None:
+                self.ammo = ammo
             damage_taken_delta = -damage_taken + self.damage_taken
             self.damage_taken = damage_taken
             hitcount_delta = hitcount - self.hitcount
@@ -41,15 +61,69 @@ class DoomEnvironment(Env):
             self.ammo = ammo
             reward = movement_reward + damage_taken_delta*10 + hitcount_delta*200  + ammo_delta*5 
             return reward
+        elif self.map_name == "deathmatch":
+            killcount, health, armor, itemCount, damageCount ,selected_weapon, ammo,x,z = game_variables
+            x,z = round(x,4), round(z,4)
+            deltaInvalid = 0
+            self.prevWeapon = selected_weapon
+            if (x == self.position[0]) and (z == self.position[1]) and action >=2 and action <= 5:
+                deltaInvalid -= 2
+            self.position[0] = x; self.position[1] = z
+            selected_weapon = int(selected_weapon)
+            if self.weapons[selected_weapon-1] == None:
+                self.weapons[selected_weapon-1] = ammo
+            curAmmo = self.weapons[selected_weapon-1]
+            deltaKill = killcount - self.hitcount
+            deltaHealth = health - self.health
+            deltaArmor = armor - self.armor
+            deltaAmmo = ammo - curAmmo
+            deltaDamageCount = damageCount - self.damageCount
+            deltaItemCount = itemCount - self.itemCount
+            self.itemCount = itemCount
+            self.damageCount = damageCount
+            self.armor = armor
+            self.health = health
+            self.weapons[selected_weapon-1] = ammo
+            if deltaHealth > 0:
+                deltaHealth *= 2
+            if deltaDamageCount == 0 and deltaKill != 0:
+                deltaKill = 0
+            self.hitcount = killcount
+            if deltaArmor > 0:
+                deltaArmor *= 2
+            reward = deltaArmor + deltaHealth + deltaKill*10 + deltaAmmo*2 + deltaDamageCount + deltaItemCount + reward + deltaInvalid
+            #print(f"{deltaAmmo=}, {deltaHealth=}, {self.hitcount=},{deltaArmor=},{deltaDamageCount=}.{reward=},{deltaItemCount=}", end='\r')
+            return reward
+        elif self.map_name == "defend_the_line":
+            ammo, health, killcount, damagecount = game_variables
+            if self.ammo == None:
+                self.ammo = ammo
+            deltaHealth = health - self.health
+            deltaAmmo = ammo - self.ammo
+            deltaKill = killcount - self.hitcount
+            deltaDamageCount = damagecount - self.damageCount
+            if deltaDamageCount == 0 and deltaKill > 0:
+                deltaKill = 0
+            self.ammo = ammo
+            self.health = health
+            self.hitcount = killcount
+            if deltaAmmo > 0:
+                deltaAmmo = 0
+            reward = deltaHealth*3 + reward + deltaKill*10 + deltaAmmo + deltaDamageCount
+            return reward
+
+
         
 
     def step(self, action):
         actions = np.identity(len(self.buttons))
-        reward = self.game.make_action(actions[action], 4) 
+        reward = self.game.make_action(actions[action],2) 
         if self.game.get_state(): 
-            state = self.game.get_state().screen_buffer
+            state = self.game.get_state().labels_buffer
             state = self.reduce_size(state)
-            reward = self.get_reward(reward)
+            #cv2.imshow("screen",state)
+            #cv2.waitKey(10)
+            reward = self.get_reward(reward,action)
             info = self.ammo
         else: 
             state = np.zeros(self.observation_space.shape)
@@ -64,15 +138,15 @@ class DoomEnvironment(Env):
         pass
     
     def reset(self): 
+        self.reset_variables()
         self.game.new_episode()
-        state = self.game.get_state().screen_buffer
+        state = self.game.get_state().labels_buffer
         state = self.reduce_size(state)
         return state
 
 
     def reduce_size(self, image):
-        shiftedGreyImage = cv2.cvtColor(np.moveaxis(image,0,-1),cv2.COLOR_BGR2GRAY)
-        resize = cv2.resize(shiftedGreyImage,(120,120),interpolation=cv2.INTER_CUBIC)
+        resize = cv2.resize(image,(120,120))
         x,y=resize.shape
         x_offset = 16
         y_offset = 0
@@ -81,12 +155,12 @@ class DoomEnvironment(Env):
         return resize
     
     @staticmethod
-    def show_difference():
-        env = DoomEnvironment()
-        state = env.game.get_state().screen_buffer
-        reduced = env.reduce_size(state)
+    def show_difference(map_name):
+        env = DoomEnvironment(map_name=map_name)
+        state = env.game.get_state()
+        reduced = env.reduce_size(state.labels_buffer)
         env.close()
-        original = np.moveaxis(state,0,-1)
+        original = np.moveaxis(state.screen_buffer,0,-1)
         fig = plt.figure(figsize=(10, 8))
         fig.add_subplot(2,2,1)
         plt.imshow(original)
